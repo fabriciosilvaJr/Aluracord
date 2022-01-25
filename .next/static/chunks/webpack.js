eeb122f24570324fabe82d7b8b6c1b971cb79f96
Fabrício Silva <fabriciosilva187@gmail.com>
2022-01-24 21:04:57 -0300

    add components skynexui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6ee5bb20c8ce85dc"; }
+/******/ 		__webpack_require__.h = function() { return "85d3747a3208b3e2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
